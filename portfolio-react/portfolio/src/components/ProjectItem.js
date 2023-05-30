f925b323774107b769dd900243010e1ea08a784a
Christy Fury <christy.fury@gmail.com>
2023-05-30 11:27:01 -0400

    adds project info, images, and links

--- portfolio-react/portfolio/src/components/ProjectItem.js
+++ portfolio-react/portfolio/src/components/ProjectItem.js
@@ -7,40 +7,47 @@ function Project() {
        <Card className="text-center">
       <Card.Header></Card.Header>
       <Card.Body>
-        <Card.Title>Project Title</Card.Title>
-        <Image src="holder.js/100px250" fluid />
+        <Card.Title>Echo</Card.Title>
+        <Image src="https://i.imgur.com/jLQ2Q2i.png" style={{width: "600px"}} fluid />
         <Card.Text>
-          Description
+          A library for users to store youtube video links and categorize them for later re-watching. 
         </Card.Text>
+        <a href="" target="_">
         <Button variant="primary">Live Link</Button>
+        </a>
       </Card.Body>
-      <Card.Footer className="text-muted">Technologies used: </Card.Footer>
+      <Card.Footer className="text-muted">Technologies used: MERN stack</Card.Footer>
     </Card>
 
     <Card className="text-center">
       <Card.Header></Card.Header>
       <Card.Body>
-        <Card.Title>Project Title</Card.Title>
-        <Image src="holder.js/100px250" fluid />
+        <Card.Title>Training With Fury</Card.Title>
+        <Image src="https://i.imgur.com/mYzij9X.png" style={{width: "600px", margin: "12px"}} fluid />
+        <Image src="https://i.imgur.com/OhA01TC.png" style={{width: "600px"}} fluid />
         <Card.Text>
-          Description
+          A web application to create, store, and track exercises and workouts. 
         </Card.Text>
+        <a href="https://training-with-fury.onrender.com/" target="_">
         <Button variant="primary">Live Link</Button>
+        </a>
       </Card.Body>
-      <Card.Footer className="text-muted">Technologies used: </Card.Footer>
+      <Card.Footer className="text-muted">Technologies used: Mongo, Mongoose, Node.js, JavaScript, HTML, EJS, CSS</Card.Footer>
     </Card>
        
     <Card className="text-center">
       <Card.Header></Card.Header>
       <Card.Body>
-        <Card.Title>Project Title</Card.Title>
-        <Image src="holder.js/100px250" fluid />
+        <Card.Title>Escape</Card.Title>
+        <Image src="https://camo.githubusercontent.com/0d42b6cc3f1a3976d7909942b2f0107e82284376f7046aa6839449cfb64f1ae8/68747470733a2f2f692e696d6775722e636f6d2f576e43464979522e706e67" style={{width: "600px"}} fluid />
         <Card.Text>
-          Description
+          A click based web application game in the style of an escape room. Demonstrates DOM manipulation, javascript, CSS and HTML proficiency. 
         </Card.Text>
+        <a href="https://christyfury10.github.io/escape-game-repo/" target="_">
         <Button variant="primary">Live Link</Button>
+        </a>
       </Card.Body>
-      <Card.Footer className="text-muted">Technologies used: </Card.Footer>
+      <Card.Footer className="text-muted">Technologies used: JavaScript, HTML, CSS, Jquery</Card.Footer>
     </Card>
       </div>
     );
